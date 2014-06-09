a63410d2f0dd05b5d9f5468dae5cad05fe898986
Yuri Arabadji <yuri@fused.com>
2014-06-09 20:39:45 +0300

    stupid fix

--- lib/cpanelhelper/config.rb
+++ lib/cpanelhelper/config.rb
@@ -1,41 +1,43 @@
 require 'ostruct'
 
 module CPanelHelper
-	class Config
-		# CPanel access hash, takes precedence over password auth
-		attr_accessor :access_hash
-		# Logger derivative
-		attr_accessor :logger
+  class Config
+    # CPanel access hash, takes precedence over password auth
+    attr_accessor :access_hash
+    # Logger derivative
+    attr_accessor :logger
 
-		# Auth username
-		attr_accessor :user
-		attr_accessor :password
+    # Auth username
+    attr_accessor :user
+    attr_accessor :password
 
-		# WHM API URI, with proto and port
-		attr_accessor :uri_host
+    # WHM API URI, with proto and port
+    attr_accessor :uri_host
 
-		# Call encoding
-		attr_accessor :call_type
+    # Call encoding
+    attr_accessor :call_type
 
-		# Domain data file
-		attr_accessor :cpanel_domain_data_file
+    # Domain data file
+    attr_accessor :cpanel_domain_data_file
 
-		attr_accessor :cpanel_user_data_dir
+    attr_accessor :cpanel_domain_ips_file
 
-		attr_reader :cpanel_mkaccesshash_exe
+    attr_accessor :cpanel_user_data_dir
 
-		# installed SSL certs DB file
-		attr_accessor :ssl_certs_db
+    attr_reader :cpanel_mkaccesshash_exe
 
-		# Set default values
-		def initialize
-			@cpanel_domain_data_file = '/etc/userdatadomains'
+    # installed SSL certs DB file
+    attr_accessor :ssl_certs_db
+
+    # Set default values
+    def initialize
+      @cpanel_domain_data_file = '/etc/userdatadomains'
       @cpanel_domain_ips_file  = '/etc/domainips'
-			@cpanel_user_data_dir    = '/var/cpanel/users'
-			@cpanel_mkaccesshash_exe = '/usr/local/cpanel/bin/mkaccesshash'
-			@ssl_certs_db            = '/var/cpanel/ssl/installed/ssl.db'
+      @cpanel_user_data_dir    = '/var/cpanel/users'
+      @cpanel_mkaccesshash_exe = '/usr/local/cpanel/bin/mkaccesshash'
+      @ssl_certs_db            = '/var/cpanel/ssl/installed/ssl.db'
 
-			@call_type = :json
-		end
-	end
+      @call_type = :json
+    end
+  end
 end
